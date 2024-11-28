4ecf20c1d45cd669165ee874b43abaf5790cf709
yannpradel <yann.pradel1@gmail.com>
2024-11-28 12:14:09 +0100

    add-new version for test raspberry

--- fps.gd
+++ fps.gd
@@ -1,7 +1,34 @@
 extends Node
 
+var udp_socket = UDPServer.new()
+var port = 12345  # Port pour le socket UDP
 @onready var fps_label = $FPSLabel  # Assurez-vous que le chemin soit correct
 
+func _ready():
+	if udp_socket.listen(port) != OK:
+		print("Impossible de démarrer le serveur UDP")
+		return
+	print("Serveur UDP démarré sur le port %d" % port)
+
 func _process(delta):
-	# Calcule et affiche les FPS
+		# Calcule et affiche les FPS
 	fps_label.text = "FPS: " + str(Engine.get_frames_per_second())
+	while udp_socket.is_connection_available():
+		var packet = udp_socket.get_packet()
+		var message = packet.get_string_from_utf8()
+		if message == "BUTTON_1_PRESSED":
+			handle_button_1()
+		elif message == "BUTTON_2_PRESSED":
+			handle_button_2()
+
+func handle_button_1():
+	print("Bouton 1 pressé !")
+	# Ajoutez ici la logique pour le bouton 1, par exemple :
+	# Déplacer un personnage à gauche
+	pass
+
+func handle_button_2():
+	print("Bouton 2 pressé !")
+	# Ajoutez ici la logique pour le bouton 2, par exemple :
+	# Déplacer un personnage à droite
+	pass
