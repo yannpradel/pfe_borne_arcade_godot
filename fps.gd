a87c5640e0ddb76f9feeb74fc7dda699df24ad31
yannpradel <yann.pradel1@gmail.com>
2024-11-28 13:25:48 +0100

    new version udp

--- fps.gd
+++ fps.gd
@@ -2,7 +2,6 @@ class_name ServerNode
 extends Node
 
 var server := UDPServer.new()
-var peers = []  # Liste des pairs connectés
 
 func _ready():
 	var port = 12345
@@ -15,21 +14,13 @@ func _ready():
 
 func _process(delta):
 	server.poll()  # Nécessaire pour vérifier les connexions entrantes
-	
+
 	# Vérifie si une connexion est disponible
-	if server.is_connection_available():
+	while server.is_connection_available():
 		var peer: PacketPeerUDP = server.take_connection()
 		if peer:
 			var packet = peer.get_packet()
-			print("Connexion acceptée : %s:%d" % [peer.get_packet_ip(), peer.get_packet_port()])
-			print("Données reçues : %s" % [packet.get_string_from_utf8()])
-			# Répond au pair pour confirmer la réception
+			print("Connexion reçue : %s:%d" % [peer.get_packet_ip(), peer.get_packet_port()])
+			print("Message reçu : %s" % [packet.get_string_from_utf8()])
+			# Répond directement au client
 			peer.put_packet(packet)
-			# Ajoute le pair à la liste pour les communications ultérieures
-			peers.append(peer)
-	
-	# Traite les pairs déjà connectés
-	for i in range(peers.size()):
-		var peer = peers[i]
-		# Exemple : Envoi d'un message périodique (remplacer par votre logique)
-		# peer.put_packet("Ping depuis le serveur".to_utf8())
