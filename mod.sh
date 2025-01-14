63d6d9c5c531bdd57fb1b6ed173aecfdaaec4647
yannpradel <96725028+yannpradel@users.noreply.github.com>
2025-01-14 17:10:59 +0100

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -3,9 +3,9 @@
 git pull
 sleep 3  # Pause de 3 secondes après le pull
 
-chmod +x Squash\ the\ Creeps\ \(3D\)V2.arm64
-chmod +x Squash\ the\ Creeps\ \(3D\)V2.sh
-chmod +x Squash\ the\ Creeps\ \(3D\)V2.pck
+chmod +x Squash\ the\ Creeps\ \(3D\).arm64
+chmod +x Squash\ the\ Creeps\ \(3D\).sh
+chmod +x Squash\ the\ Creeps\ \(3D\).pck
 
 sleep 3  # Pause de 3 secondes avant le lancement
 
