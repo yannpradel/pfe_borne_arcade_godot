90240862da1d17e93f81e9755b88f5cb23d9c5c0
yannpradel <yann.pradel1@gmail.com>
2025-01-17 16:19:01 +0100

    calibrage avec joystick

--- python udp/tcp_for_serial.py
+++ python udp/tcp_for_serial.py
@@ -1,9 +1,8 @@
 import serial
 import socket
-import time
 
 # Configuration du port série et de la connexion TCP
-serial_port = 'COM4'  # Port série correct pour Arduino Leonardo
+serial_port = 'COM3'  # Port série correct pour Arduino Leonardo
 baud_rate = 9600
 server_ip = '127.0.0.1'
 server_port = 12345
@@ -11,7 +10,7 @@ server_port = 12345
 print(f"Initialisation du port série : {serial_port} à {baud_rate} bauds")
 # Initialisation du port série
 try:
-    ser = serial.Serial(serial_port, baud_rate)
+    ser = serial.Serial(serial_port, baud_rate, timeout=1)
     print("Port série initialisé avec succès.")
 except serial.SerialException as e:
     print(f"Erreur lors de l'initialisation du port série : {e}")
@@ -32,18 +31,17 @@ try:
     while True:
         if ser.in_waiting > 0:
             # Lire les données du port série
-            data = ser.readline().decode('utf-8', errors='replace').strip()
+            data = ser.readline().decode('utf-8').strip()  
             print(f"DEBUG - Données brutes reçues : {data}")
-
-            # Envoyer les données à Godot via TCP
+            
+            # Envoyer les données brutes à Godot via TCP
             sock.sendall(data.encode('utf-8'))
-            print(f"Envoyé à Godot: {data}")
-        
-        time.sleep(0.1)
+            print(f"Envoyé à Godot : {data}")
 
 except KeyboardInterrupt:
     print("Fermeture du programme.")
-finally:
+finally:     
     ser.close()
     sock.close()
     print("Ports série et TCP fermés.")
+    
