dae0aa59c20caa7732de696b9eaa65682ccd10b9
unknown <duhoo.nathan@gmail.com>
2025-01-21 15:14:46 +0100

    Barnaby add

--- windows.gd
+++ windows.gd
@@ -10,6 +10,6 @@ func _ready():
 	# Configurer une deuxième fenêtre en plein écran
 	var window = $Window
 	window.size = Vector2(1920, 1080)  # Taille de la deuxième fenêtre
-	window.position = Vector2(0,1080)  # Position sur le deuxième écran
 	window.mode = Window.MODE_FULLSCREEN  # Activer le plein écran pour la deuxième fenêtre
+	window.set_current_screen(1)
 	window.show()
