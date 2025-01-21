f1633e00956173ac4ced2f46a4849fdd84b2f3a9
yannpradel <yann.pradel1@gmail.com>
2025-01-21 16:50:52 +0100

    add auto bounce when poison

--- losing_life.gd
+++ losing_life.gd
@@ -1,11 +1,19 @@
 extends Area3D
 
 func _ready():
-	# Connexion du signal `body_entered`
+	# Connexion des signaux `body_entered` et `body_exited`
 	connect("body_entered", self._on_body_entered)
+	connect("body_exited", self._on_body_exited)
 	
 func _on_body_entered(body: Node3D) -> void:
 	# Vérifie si l'objet détecté est le joueur
 	if body.name == "Player":
-		print("PERTE DE VIE : Le joueur est entré dans la zone !")
+		print("PERTE DE VIE : Le joueur est entré dans la lave !")
 		body.lose_life()  # Appelle la méthode lose_life() du joueur
+		body.enter_lava()  # Active le saut continu
+
+func _on_body_exited(body: Node3D) -> void:
+	# Vérifie si l'objet détecté est le joueur
+	if body.name == "Player":
+		print("Le joueur est sorti de la lave.")
+		body.exit_lava()  # Désactive le saut continu
