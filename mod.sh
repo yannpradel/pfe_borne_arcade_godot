19dce812df8fb36f6ac0b0a475740a77e45e1a30
unknown <duhoo.nathan@gmail.com>
2025-01-22 17:57:44 +0100

    Executable

--- mod.sh
+++ mod.sh
@@ -3,10 +3,10 @@
 git pull
 sleep 3  # Pause de 3 secondes après le pull
 
-chmod +x BillieBustUpArcade.arm64
-chmod +x BillieBustUpArcade.sh
-chmod +x BillieBustUpArcade.pck
+chmod +x export/BillieBustUpArcade.arm64
+chmod +x export/BillieBustUpArcade.sh
+chmod +x export/BillieBustUpArcade.pck
 
 sleep 3  # Pause de 3 secondes avant le lancement
 
-./BillieBustUpArcade.sh
+.export/BillieBustUpArcade.sh
