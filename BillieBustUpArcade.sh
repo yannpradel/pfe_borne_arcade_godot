f7f7971a902dc2cb5ad7cbf307276ac4e98df946
unknown <duhoo.nathan@gmail.com>
2025-01-22 17:19:38 +0100

    V 0.1

--- BillieBustUpArcade.sh
+++ BillieBustUpArcade.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo -ne '\033c\033]0;Squash the Creeps (3D)\a'
+echo -ne '\033c\033]0;BillieBustUpArcade\a'
 base_path="$(dirname "$(realpath "$0")")"
-"$base_path/Squash the Creeps (3D).arm64" "$@"
+"$base_path/BillieBustUpArcade.arm64" "$@"
