8d5740d1113355ba44339e8e4fe77b86c64416f4
unknown <duhoo.nathan@gmail.com>
2025-01-28 11:27:21 +0100

    Window management

--- scripts_godot/windows.gd
+++ scripts_godot/windows.gd
@@ -9,7 +9,8 @@ func _ready():
 
 	# Configurer une deuxième fenêtre en plein écran
 	var window = $Window
+	window.name = "Barnaby"
 	window.size = Vector2(1920, 1080)  # Taille de la deuxième fenêtre
 	window.mode = Window.MODE_FULLSCREEN  # Activer le plein écran pour la deuxième fenêtre
-	window.set_current_screen(1)
+	window.set_current_screen(0)
 	window.show()
