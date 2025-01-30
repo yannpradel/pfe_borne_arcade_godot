368b325ba12d53c0d18bc32ce5df23cdbfcf33df
unknown <duhoo.nathan@gmail.com>
2025-01-30 11:40:47 +0100

    Resolution back screen

--- scripts_godot/windows.gd
+++ scripts_godot/windows.gd
@@ -7,7 +7,7 @@ func _ready():
 	# Configurer une deuxième fenêtre en plein écran
 	var window = $Window
 	window.name = "Barnaby"
-	window.size = Vector2(1920, 1080)  # Taille de la deuxième fenêtre
+	window.size = Vector2(1366, 768)  # Taille de la deuxième fenêtre
 	window.mode = Window.MODE_FULLSCREEN  # Activer le plein écran pour la deuxième fenêtre
 	window.set_current_screen(0)
 	window.show()
